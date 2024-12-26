df43b0099055257235d37230d9b90130eec802fc
JATIN-RAGHAV <jrag137@gmail.com>
2024-12-27 00:14:35 +0530

    Added enquiry form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,26 @@ import Enquiry from "./ui/Enquiry";
 export default function Home() {
   return (
     <div>
-      <Enquiry />
+      <div className="overflow-hidden">
+        <ImageBanner />
+        <ThingsCounter />
+        <div className="flex justify-center py-2">
+          <div className="h-[2px] bg-gray-300 w-11/12" />
+        </div>
+        <FeaturedProductsServer />
+        <div className="flex justify-center py-2">
+          <div className="h-[2px] bg-gray-300 w-11/12" />
+        </div>
+        <CategoryIconRow />
+        <div className="flex justify-center py-2">
+          <div className="h-[2px] bg-gray-300 w-11/12" />
+        </div>
+        <OurParteners />
+        <div className="flex justify-center py-2">
+          <div className="h-[2px] bg-gray-300 w-11/12" />
+        </div>
+        <Enquiry />
+      </div>
     </div>
   );
 }
--- src/app/ui/Enquiry.tsx
+++ src/app/ui/Enquiry.tsx
@@ -2,14 +2,20 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 
-const App = () => {
+const Enquiry = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: '',
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleChangeTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
@@ -28,6 +34,11 @@ const App = () => {
       .then((response) => {
         alert('Email sent successfully!');
         console.log('Success:', response.status, response.text);
+        setFormData({
+          name: '',
+          email: '',
+          message: ''
+        })
       })
       .catch((err) => {
         alert('Failed to send email.');
@@ -36,27 +47,38 @@ const App = () => {
   };
 
   return (
-    <form onSubmit={sendEmail}>
-      <input
-        type="text"
-        name="name"
-        placeholder="Your Name"
-        onChange={handleChange}
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="Your Email"
-        onChange={handleChange}
-      />
-      <input
-        name="message"
-        placeholder="Your Message"
-        onChange={handleChange}
-      />
-      <button type="submit">Send Email</button>
-    </form>
+    <div className='w-full flex items-center flex-col'>
+      <div className='flex items-center font-bold justify-center text-4xl p-1 box-border'>
+        Enquiry Form
+      </div>
+      <form onSubmit={sendEmail} className='w-full md:w-1/2 lg:w-1/3 flex flex-col bg-gray-200 items-center gap-6 my-4 py-6 rounded-lg'>
+        <input
+          type="text"
+          name="name"
+          placeholder="Your Name"
+          className='w-3/4 rounded-lg px-2 py-1'
+          value={formData.name}
+          onChange={handleChangeInput}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          className='w-3/4 rounded-lg px-2 py-1'
+          value={formData.email}
+          onChange={handleChangeInput}
+        />
+        <textarea
+          name="message"
+          placeholder="Your Message"
+          onChange={handleChangeTextarea}
+          value={formData.message}
+          className='h-[200px] rounded-lg w-3/4 px-2'
+        />
+        <button type="submit" className='w-1/4 bg-themeBlue rounded-lg h-[30px] text-white hover:bg-white hover:text-themeBlue transition-all'>Send Email</button>
+      </form>
+    </div>
   );
 };
 
-export default App;
+export default Enquiry;
